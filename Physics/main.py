415f58da319eee56aed58b1d858db4b8e794f5d3
Matteo-05 <matteo.sulejmani.2023@wat.al>
2023-01-21 22:12:22 +0100

    Fixed some math function plotting

--- Physics/main.py
+++ Physics/main.py
@@ -1,5 +1,9 @@
-from Electromagenetism import *
+import electromagnetism
 from conversions import *
 
 if __name__ == "__main__":
     print(electromagnetism.emfMoving(2.5, 5, 3))
+
+'''
+!! Temporary file
+'''
---
+++ Physics/quantumPhysics.py
@@ -0,0 +1,26 @@
+## Library imports for the package
+import math
+import json
+
+## Standard imports from the constants and units files
+with open("Constants.json", "r") as f:
+    const= json.load(f)
+
+with open("units.json", "r") as f:
+    units = json.load(f)
+
+## Functions related to quantum physics
+def photoelectric():
+    pass
+
+def electronEnergy(frequency:float) ->float:
+    return const['planckConst'] * frequency
+
+def einsteinPhotoelecticEq(workFunction: float, electronEnergy: float=0, frequency: float=0) -> float:
+    if electronEnergy:
+        return electronEnergy - workFunction
+    
+    return (frequency * const['planckConst']) - workFunction
+
+def bohrOrbitEnergy(n: int) -> float:
+    return (-13.6 / pow(n, 2))
